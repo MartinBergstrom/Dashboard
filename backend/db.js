2d591aa2cf664b0e7369d472e8f1ad5190ed39c1
Martin Bergström <emarbe02@gmail.com>
2023-07-30 20:53:18 +0200

    Add initial db

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const MONGO_URI = `mongodb://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@mongo:27017`;
+const dbName = "dashboard";
+const MONGO_URI =
+  `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:
+${process.env.MONGO_INITDB_ROOT_PASSWORD}@mongo:27017/` + dbName;
 
 const connectDB = async () => {
   await mongoose
@@ -8,9 +11,9 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
-    .then(() => console.log("Mongo connected successfully"))// Logs out successful when MongoDB connects.
+    .then(() => console.log("Mongo connected successfully")) // Logs out successful when MongoDB connects.
     .catch((e) => {
-      console.log(e.message);// Logs out the error message if it encounters any.
+      console.log(e.message); // Logs out the error message if it encounters any.
     });
 };
 
