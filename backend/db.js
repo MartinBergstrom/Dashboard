3f71186b61911f7ff2f50a2ac14766c67b8a3da9
Martin Bergström <emarbe02@gmail.com>
2023-10-04 17:40:05 +0200

    Setup pymongo

--- backend/db.js
+++ backend/db.js
@@ -2,19 +2,29 @@ const mongoose = require("mongoose");
 
 const dbName = "dashboard";
 const MONGO_URI =
-  `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:
-${process.env.MONGO_INITDB_ROOT_PASSWORD}@mongo:27017/` + dbName;
+  `mongodb://${process.env.MONGO_NODE_USERNAME}:
+${process.env.MONGO_NODE_PASSWORD}@mongo:27017/` + dbName;
 
 const connectDB = async () => {
-  await mongoose
-    .connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => console.log("Mongo connected successfully")) // Logs out successful when MongoDB connects.
-    .catch((e) => {
-      console.log(e.message); // Logs out the error message if it encounters any.
-    });
+  try {
+    await mongoose
+      .connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        console.log("Mongo connected successfully");
+      }) // Logs out successful when MongoDB connects.
+      .catch((e) => {
+        console.log(e.message); // Logs out the error message if it encounters any.
+      });
+
+    const collection = mongoose.connection.db.collection("urlcards");
+    const data = await collection.find({}).toArray();
+    console.log("Collection data:", data);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+  }
 };
 
 module.exports = connectDB;
--- backend/python/nxtwatch/app.py
+++ backend/python/nxtwatch/app.py
@@ -1,13 +1,46 @@
-import pymongo
-from flask import Flask
+from pymongo import MongoClient
+import urllib.parse
+from flask import Flask, g
+import json
 import os
+from bson import json_util
 
 app = Flask(__name__)
+
+username = urllib.parse.quote_plus(os.getenv('MONGO_NXTWATCH_USERNAME'))
+password = urllib.parse.quote_plus(os.environ.get('MONGO_NXTWATCH_PASSWORD'))
+app.config['MONGO_URI'] = f"mongodb://{username}:{password}@mongo:27017/nxtwatch"
+
+
+def parse_json(data):
+    return json.loads(json_util.dumps(data))
+
+
+def get_db_client():
+    if 'mongo' not in g:
+        g.mongo = MongoClient(app.config['MONGO_URI'])
+    return g.mongo
 
 
 @app.route("/")
 def hello_world():
     return "Hello, World!xd1"
+
+
+@app.route("/watches", methods=['GET'])
+def get_watches():
+    client = get_db_client()
+    db_nxtwatch = client['nxtwatch']
+    collection = db_nxtwatch.get_collection('watches')
+    data = list(collection.find({}))
+    return parse_json(data)
+
+
+@app.teardown_appcontext
+def close_mongo_connection(error):
+    mongo = g.pop('mongo', None)
+    if mongo is not None:
+        mongo.close()
 
 
 if __name__ == "__main__":
