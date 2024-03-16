fb0ff224228931afd58b89f3b1bff84f88f8710f
Martin Bergström <emarbe02@gmail.com>
2024-03-16 19:36:51 +0100

    Add opacity on comments text area

--- frontend/src/components/apps/nxtwatch/comments/Comments.tsx
+++ frontend/src/components/apps/nxtwatch/comments/Comments.tsx
@@ -4,14 +4,16 @@ import { getComment, updateCommentText } from "../service/NxtwatchService";
 import { useMutation, useQuery } from "react-query";
 import { CommentModel } from "../model/CommentModel";
 
-
 const CommentsArea = () => {
-    const DEFAULT_ROWS = 1;
+    const DEFAULT_ROWS = 2;
     const EXPANDED_ROWS = 20;
-    const DEFAULT_WIDTH = "15%";
+    const DEFAULT_WIDTH = "25%";
     const EXPANDED_WIDTH = "60%";
+    const DEFAULT_OPACITY = "0.5";
+    const FULL_OPACITY = "1";
     const [rows, setRows] = useState(DEFAULT_ROWS);
     const [width, setWidth] = useState(DEFAULT_WIDTH);
+    const [opacity, setOpacity] = useState(DEFAULT_OPACITY);
     const [currentText, setCurrentText] = useState<string>("");
     const textareaRef = useRef<HTMLDivElement>(null)
     const targetRef = useRef<HTMLDivElement>(null);
@@ -19,10 +21,10 @@ const CommentsArea = () => {
     const { isLoading } = useQuery<CommentModel>(
         "watchescomment",
         getComment, {
-            onSuccess: (data) => {
-                setCurrentText(data.text);
-            }
+        onSuccess: (data) => {
+            setCurrentText(data.text);
         }
+    }
     );
 
     const { mutate: mutatePut } = useMutation(
@@ -47,11 +49,13 @@ const CommentsArea = () => {
     const onTextFieldFocus = () => {
         setRows(EXPANDED_ROWS);
         setWidth(EXPANDED_WIDTH);
+        setOpacity(FULL_OPACITY)
     }
 
     const onTextFieldBlur = () => {
         setRows(DEFAULT_ROWS);
         setWidth(DEFAULT_WIDTH);
+        setOpacity(DEFAULT_OPACITY);
         mutatePut(currentText);
     }
 
@@ -65,7 +69,7 @@ const CommentsArea = () => {
 
     return (
         <div
-            style={{ width: width, margin: "auto", marginBottom: "50px" }}>
+            style={{ width: width, margin: "auto", marginBottom: "50px", overflow: "none", opacity: opacity }}>
             <TextField
                 ref={textareaRef}
                 onFocus={onTextFieldFocus}
