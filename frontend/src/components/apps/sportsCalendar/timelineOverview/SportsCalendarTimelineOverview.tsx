274e5546e37023e2a5b8360fa2341188d41fb5fc
Martin Bergström <emarbe02@gmail.com>
2024-04-13 10:29:07 +0200

    Add pseudo code for info view

--- frontend/src/components/apps/sportsCalendar/timelineOverview/SportsCalendarTimelineOverview.tsx
+++ frontend/src/components/apps/sportsCalendar/timelineOverview/SportsCalendarTimelineOverview.tsx
@@ -1,10 +1,29 @@
+import { SportsCalendarEvent } from "../model/SportsCalendarModels";
 import "./SportsCalendarTimelineOverview.css";
 
-interface SportsCalendarTimelineOverviewProps {}
+interface SportsCalendarTimelineOverviewProps {
+  allEvents?: SportsCalendarEvent[];
+
+}
 
 const SportsCalendarTimelineOverview = (
   props: SportsCalendarTimelineOverviewProps
 ) => {
+
+  const sortAllEvents =() => {
+    // Sort all events
+  }
+
+  const renderTodaysEvents =() => {
+    // Render seperate section for todays event
+  }
+
+  const renderEvents = () => {
+    const sorted = sortAllEvents();
+    // use sorted and create List view first
+    // 
+  }
+
   return (
     <>
       <div className="event-timeline-overview">HELLO</div>
