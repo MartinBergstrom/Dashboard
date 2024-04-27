7d88f221e4fc022bcf1b554b7dd0fb89c4aef60b
Martin Bergström <emarbe02@gmail.com>
2024-04-27 12:13:35 +0200

    Fix drag to scroll in highlights view

--- frontend/src/components/apps/sportsCalendar/infoView/InfoViewHighlights.tsx
+++ frontend/src/components/apps/sportsCalendar/infoView/InfoViewHighlights.tsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from "react";
 import { EventHighlight } from "../model/SportsCalendarModels";
 import "./InfoViewHighlights.css";
 
@@ -5,16 +6,64 @@ interface InfoViewHighlightsProps {
   hightlights?: EventHighlight[];
 }
 
-// Ideas here
-// Slideshow with cards? One card is squre (calendar square) with info about highlight
 const InfoViewHighlights = (props: InfoViewHighlightsProps) => {
+  const ourRef = useRef<HTMLDivElement>(null);
+  const [isMouseDown, setIsMouseDown] = useState(false);
+  const mouseCoords = useRef({
+    startX: 0,
+    scrollLeft: 0,
+  });
+
+  const handleDragStart = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (!ourRef.current) return;
+    const slider = ourRef.current;
+    const startX = e.pageX - slider.offsetLeft;
+    const scrollLeft = slider.scrollLeft;
+    mouseCoords.current = { startX, scrollLeft };
+    setIsMouseDown(true);
+    document.body.style.cursor = "grabbing";
+  };
+
+  const handleDragEnd = () => {
+    setIsMouseDown(false);
+    document.body.style.cursor = "default";
+  };
+
+  const handleDrag = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (!isMouseDown || !ourRef.current) return;
+    e.preventDefault();
+    const slider = ourRef.current;
+    const sliderBounds = slider.getBoundingClientRect();
+    // Stop scrolling if out of bounds
+    if (
+      e.clientX < sliderBounds.left + 2 ||
+      e.clientX > sliderBounds.right - 2 ||
+      e.clientY < sliderBounds.top + 2 ||
+      e.clientY > sliderBounds.bottom - 2
+    ) {
+      setIsMouseDown(false);
+      document.body.style.cursor = "default";
+      return;
+    }
+    const x = e.pageX - slider.offsetLeft;
+    const walkX = (x - mouseCoords.current.startX) * 2;
+    slider.scrollLeft = mouseCoords.current.scrollLeft - walkX;
+  };
+
   if (!props.hightlights || props.hightlights.length === 0) {
     return null;
   }
+
   return (
     <>
       <div className="hightlights-header">Highlights</div>
-      <div className="hightlights-main">
+      <div
+        className="hightlights-main"
+        ref={ourRef}
+        onMouseDown={handleDragStart}
+        onMouseUp={handleDragEnd}
+        onMouseMove={handleDrag}
+      >
         {props.hightlights.map((highlight, index) => (
           <div key={index} className="calendar-square">
             hello
